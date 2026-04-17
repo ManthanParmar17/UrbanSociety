b58b5b3d79f44ef36b64a127e4c89d44b2a759ea
Manthan Parmar <mnthn117@gmail.com>
2026-04-17 14:24:22 +0530

    fixed db

--- Urban Soc/backend/db.py
+++ Urban Soc/backend/db.py
@@ -1,16 +1,17 @@
 import mysql.connector
 import os
 
-try:
-    conn = mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME"),
-        port=int(os.getenv("DB_PORT"))
-    )
-
-    print("✅ MySQL Connected")
-
-except Exception as e:
-    print("❌ Error:", e)
+def get_db():
+    try:
+        conn = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME"),
+            port=int(os.getenv("DB_PORT"))
+        )
+        print("✅ MySQL Connected")
+        return conn
+    except Exception as e:
+        print("❌ DB Error:", e)
+        return None
