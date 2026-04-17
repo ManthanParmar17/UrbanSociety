6010a2b0e6b1b3ad093cdd062ce201432392b3d7
Manthan Parmar <mnthn117@gmail.com>
2026-04-17 14:18:20 +0530

    fixed port for deployment

--- Urban Soc/backend/db.py
+++ Urban Soc/backend/db.py
@@ -1,12 +1,16 @@
 import mysql.connector
 import os
 
-conn = mysql.connector.connect(
-    host=os.getenv("DB_HOST"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    database=os.getenv("DB_NAME"),
-    port=int(os.getenv("DB_PORT"))
-)
+try:
+    conn = mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT"))
+    )
 
-print("✅ MySQL Connected")
+    print("✅ MySQL Connected")
+
+except Exception as e:
+    print("❌ Error:", e)
