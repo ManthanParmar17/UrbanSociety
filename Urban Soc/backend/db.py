ee12dafd875ca7c447fa9e6395a3fcc1a8dcddf7
Manthan Parmar <mnthn117@gmail.com>
2026-04-17 14:17:15 +0530

    fixed port for deployment

--- Urban Soc/backend/db.py
+++ Urban Soc/backend/db.py
@@ -1,17 +1,12 @@
 import mysql.connector
 import os
 
-# ======================== COMMENT ========================
-# Provide a new MySQL connection for backend routes (update creds as needed).
-import os
-import mysql.connector
-
 conn = mysql.connector.connect(
-    host=os.getenv("mysql.railway.internal"),
-    user=os.getenv("root"),
-    password=os.getenv("snCqdlNByrwgtrjpbyTKLMFKFwUjXfis"),
-    database=os.getenv("railway"),
-    port=int(os.getenv("3306"))
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME"),
+    port=int(os.getenv("DB_PORT"))
 )
 
 print("✅ MySQL Connected")
